e028ec500489735e1175f49cec427c2e6da45e79
AD9680 <AD9680@student.jamk.fi>
2024-08-27 20:31:53 +0300

    update popular component

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import "./Popular.css"
+import items from '../Assets/data'
+import Item from '../Item/Item'
 
 const Popular = () => {
-  return (
-    <div className='popular'></div>
-  )
+    return (
+        <div className='popular'>
+            <h1>POPULAR IN WOMEN</h1>
+            <hr />
+                <div className="popular-item">
+                    {items.map((item, i) => {
+                        return <Item key={i} child_item={item} />
+                    })}
+                </div>
+        </div>
+    )
 }
 
 export default Popular
