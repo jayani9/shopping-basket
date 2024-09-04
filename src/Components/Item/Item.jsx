e239114e6448b4fb31b1c1e26b452bf33f1f74a8
AD9680 <AD9680@student.jamk.fi>
2024-09-04 12:58:06 +0300

    sorted the shop categories

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -5,14 +5,14 @@ const Item = (props) => {
     console.log(props)
   return (
     <div className='item'>
-        <img src={props.child_item.image} alt="" />
-        <p>{props.child_item.name}</p>
+        <img src={props.migara.image} alt="" />
+        <p>{props.migara.name}</p>
         <div className="item-prices">
             <div className="item-price-new">
-                ${props.child_item.new_price}
+                ${props.migara.new_price}
             </div>
             <div className="item-price-old">
-                ${props.child_item.old_price}
+                ${props.migara.old_price}
             </div>
         </div>
     </div>
